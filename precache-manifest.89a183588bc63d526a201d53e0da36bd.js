12e58dd2bcc462e9e1fabd15fd1598ed27653661
Sarath T <mailtsarath@yahoo.com>
2020-01-04 00:20:27 -0600

    Updates

---
+++ precache-manifest.89a183588bc63d526a201d53e0da36bd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e52d8891673e365ba9a0",
+    "url": "/static/css/main.b4c7cc05.chunk.css"
+  },
+  {
+    "revision": "e52d8891673e365ba9a0",
+    "url": "/static/js/main.3eb9749e.chunk.js"
+  },
+  {
+    "revision": "91a0e73aa79433986776",
+    "url": "/static/js/runtime~main.a5503f5d.js"
+  },
+  {
+    "revision": "c9d947e820de96c9675e",
+    "url": "/static/js/2.70a2c6ab.chunk.js"
+  },
+  {
+    "revision": "be31d486ffcdd5302215",
+    "url": "/static/css/3.07452eb6.chunk.css"
+  },
+  {
+    "revision": "be31d486ffcdd5302215",
+    "url": "/static/js/3.621bafd1.chunk.js"
+  },
+  {
+    "revision": "f3d9e90bf843804ca809",
+    "url": "/static/css/4.4aef6361.chunk.css"
+  },
+  {
+    "revision": "f3d9e90bf843804ca809",
+    "url": "/static/js/4.7c8a175b.chunk.js"
+  },
+  {
+    "revision": "1b9a565731ba39ccaf66",
+    "url": "/static/css/5.b29e315a.chunk.css"
+  },
+  {
+    "revision": "1b9a565731ba39ccaf66",
+    "url": "/static/js/5.e564d63a.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "4c1ec8ee585ad9064c2ec69a8baa293c",
+    "url": "/index.html"
+  }
+];
