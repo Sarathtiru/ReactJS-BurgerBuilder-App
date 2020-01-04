f542dd33c58f98a54d88c3833712912bad017f51
Sarath T <mailtsarath@yahoo.com>
2020-01-04 01:21:58 -0600

    Updates

---
+++ precache-manifest.efe7edb24f67eb190ef41151894806e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "62fc2364a8fe4567d19f",
+    "url": "/ReactJS-BurgerBuilder-App/static/css/main.b4c7cc05.chunk.css"
+  },
+  {
+    "revision": "62fc2364a8fe4567d19f",
+    "url": "/ReactJS-BurgerBuilder-App/static/js/main.abd4bf9d.chunk.js"
+  },
+  {
+    "revision": "1be973be5d03a683d90e",
+    "url": "/ReactJS-BurgerBuilder-App/static/js/runtime~main.74ff5553.js"
+  },
+  {
+    "revision": "c9d947e820de96c9675e",
+    "url": "/ReactJS-BurgerBuilder-App/static/js/2.70a2c6ab.chunk.js"
+  },
+  {
+    "revision": "be31d486ffcdd5302215",
+    "url": "/ReactJS-BurgerBuilder-App/static/css/3.07452eb6.chunk.css"
+  },
+  {
+    "revision": "be31d486ffcdd5302215",
+    "url": "/ReactJS-BurgerBuilder-App/static/js/3.621bafd1.chunk.js"
+  },
+  {
+    "revision": "f3d9e90bf843804ca809",
+    "url": "/ReactJS-BurgerBuilder-App/static/css/4.4aef6361.chunk.css"
+  },
+  {
+    "revision": "f3d9e90bf843804ca809",
+    "url": "/ReactJS-BurgerBuilder-App/static/js/4.7c8a175b.chunk.js"
+  },
+  {
+    "revision": "1b9a565731ba39ccaf66",
+    "url": "/ReactJS-BurgerBuilder-App/static/css/5.b29e315a.chunk.css"
+  },
+  {
+    "revision": "1b9a565731ba39ccaf66",
+    "url": "/ReactJS-BurgerBuilder-App/static/js/5.e564d63a.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/ReactJS-BurgerBuilder-App/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "6db456630f20c8e4cc02e014f99722c9",
+    "url": "/ReactJS-BurgerBuilder-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89a183588bc63d526a201d53e0da36bd.js"
+  "/ReactJS-BurgerBuilder-App/precache-manifest.efe7edb24f67eb190ef41151894806e1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactJS-BurgerBuilder-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
